cfd960f3ddc63c305a7b248a0c3e8d5542cd34ec
coreyderosa <coreyderosa@gmail.com>
2016-10-11 22:23:28 -0600

    Moved files/printing modified date/making list of files in dir
    Have functions to move files based on extension.
    Make a list of files in directory
    Print modifed date/time

--- PyDrill_scripting_27_idle.py
+++ PyDrill_scripting_27_idle.py
@@ -9,24 +9,35 @@ import os
 import glob
 import time
 
-##def copyFilesA():
-##    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
-##    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
-##        print file
-##        shutil.move(file, destDir)
-##
-##def copyFilesB():
-##    destDir = "C:/Users/Gowenburnett/Desktop/Folder A"
-##    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder B/*.txt"):
-##        print file
-##        shutil.move(file, destDir)
-##
-##copyFilesA()
-###copyFilesB()
+def copyFilesA():
+    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
+    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
+        print file
+        shutil.move(file, destDir)
+
+
+def copyFilesB():
+    destDir = "C:/Users/Gowenburnett/Desktop/Folder A"
+    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder B/*.txt"):
+        print file
+        shutil.move(file, destDir)
+
 
 def fileInfo():
     info = os.stat('C:/Users/Gowenburnett/Desktop/Folder A/random1.txt')
     print info
     print info.st_mtime
 
-fileInfo()
+
+def makeList():
+    fileList =[]
+    fileDir = 'C:/Users/Gowenburnett/Desktop/Folder A/'
+    for files in os.listdir(fileDir):
+        fileInfo = os.stat(os.path.join(fileDir, files))
+        fileList.append([files, time.ctime(fileInfo.st_mtime)])
+    print fileList
+
+#copyFilesA()
+#copyFilesB()
+#fileInfo()
+makeList()
