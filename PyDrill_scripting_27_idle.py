0d7faee17d6398be5e76498cc406b3c8fb53640b
coreyderosa <coreyderosa@gmail.com>
2016-10-11 22:08:14 -0600

    Retrieving file info
    Printing the file info on one of the files in Folder A.  Returns
    creation time and modified time along with size and other info

--- PyDrill_scripting_27_idle.py
+++ PyDrill_scripting_27_idle.py
@@ -7,18 +7,26 @@
 import shutil
 import os
 import glob
+import time
 
-def copyFilesA():
-    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
-    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
-        print file
-        shutil.move(file, destDir)
+##def copyFilesA():
+##    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
+##    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
+##        print file
+##        shutil.move(file, destDir)
+##
+##def copyFilesB():
+##    destDir = "C:/Users/Gowenburnett/Desktop/Folder A"
+##    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder B/*.txt"):
+##        print file
+##        shutil.move(file, destDir)
+##
+##copyFilesA()
+###copyFilesB()
 
-def copyFilesB():
-    destDir = "C:/Users/Gowenburnett/Desktop/Folder A"
-    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder B/*.txt"):
-        print file
-        shutil.move(file, destDir)
+def fileInfo():
+    info = os.stat('C:/Users/Gowenburnett/Desktop/Folder A/random1.txt')
+    print info
+    print info.st_mtime
 
-#copyFilesA()
-copyFilesB()
+fileInfo()
