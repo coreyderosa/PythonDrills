103952112670970f926cdf468afc2b02806e12b1
coreyderosa <coreyderosa@gmail.com>
2016-10-11 21:45:51 -0600

    Modified the shutil move files script.  Started scripting project
    Finally found a more efficient way to move multiple files!

--- PyDrill_scripting_27_idle.py
+++ PyDrill_scripting_27_idle.py
@@ -6,6 +6,19 @@
 
 import shutil
 import os
+import glob
 
-copyFiles():
-    
+def copyFilesA():
+    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
+    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
+        print file
+        shutil.move(file, destDir)
+
+def copyFilesB():
+    destDir = "C:/Users/Gowenburnett/Desktop/Folder A"
+    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder B/*.txt"):
+        print file
+        shutil.move(file, destDir)
+
+copyFilesA()
+#copyFilesB()
--- PyDrill_shutil_module_27_idle.py
+++ PyDrill_shutil_module_27_idle.py
@@ -6,22 +6,12 @@
 
 import shutil
 import os
+import glob
 
 def moveFiles():
-    file1 = "C:\\Users\\Gowenburnett\\Desktop\\Folder A\\random1.txt"
-    file2 = "C:\\Users\\Gowenburnett\\Desktop\\Folder A\\random2.txt"
-    file3 = "C:\\Users\\Gowenburnett\\Desktop\\Folder A\\random3.txt"
-    file4 = "C:\\Users\\Gowenburnett\\Desktop\\Folder A\\random4.txt"
-    dst = "C:\\Users\\Gowenburnett\\Desktop\\Folder B"
-    shutil.move(file1,dst)
-    shutil.move(file2,dst)
-    shutil.move(file3,dst)
-    shutil.move(file4,dst)
-    print os.path.realpath(file1)
-    print os.path.realpath(file2)
-    print os.path.realpath(file3)
-    print os.path.realpath(file4)
+    destDir = "C:/Users/Gowenburnett/Desktop/Folder B"
+    for file in glob.glob(r"C:/Users/Gowenburnett/Desktop/Folder A/*.txt"):
+        print file
+        shutil.move(file, destDir)
 
 moveFiles()
-
-
