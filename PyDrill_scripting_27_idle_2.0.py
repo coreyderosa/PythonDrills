f821e3fbcbf8863a9ec8067205d5439b440135e6
coreyderosa <coreyderosa@gmail.com>
2016-10-16 18:41:49 -0600

    Updating script with txt filter
    Function now filters .txt files and files modified with 24 hours.  Also,
    allows source folder to be selected without adding '*.txt' to fileSrc.
    This helps user select the folder instead of having to choose individual
    txt files.

--- PyDrill_scripting_27_idle_2.0.py
+++ PyDrill_scripting_27_idle_2.0.py
@@ -14,14 +14,17 @@ from glob import glob
 def timeCompare(fileSrc, fileDest):
     currentTime = datetime.now() #gives us the current time
     time24hoursAgo = currentTime - timedelta(hours = 24) #finds the time 24 hours from current time
-    for files in glob(fileSrc): #glob shows the entire file path
-        fileSrcModifiedTime = datetime.fromtimestamp(os.path.getmtime(files)) #gives us each file's modified time
-        if fileSrcModifiedTime > time24hoursAgo: #if file's modified time is greater than 24 hours old copy file to destination folder
-            print files, "copied to: ", fileDest
-            shutil.copy(files,fileDest) #copys file to destination
-        else:
-            print files, "not copied"
+    for f in os.listdir(fileSrc): #glob shows the entire file path
+        files = os.path.realpath(os.path.join(fileSrc,f)) #shows the file's path then joins the file's path and the directory's paths.  This provides the datetime with the entire file's path
+        if files.endswith('.txt'):
+            fileSrcModifiedTime = datetime.fromtimestamp(os.path.getmtime(files)) #gives us each file's modified time
+            if fileSrcModifiedTime > time24hoursAgo: #if file's modified time is greater than 24 hours old copy file to destination folder
+                print files, "copied to: ", fileDest
+                shutil.copy(files,fileDest) #copys file to destination
+            else:
+                print files, "not copied"
 
-fileSrc = r'D:\Documents\Folder A\*txt'
-fileDest = r'D:\Documents\Folder B'
+fileSrc = 'D:\Documents\Folder A\*.txt'
+#fileSrc = 'D:\Documents\Folder A'            
+fileDest = 'D:\Documents\Folder B'
 timeCompare(fileSrc, fileDest) #2 arguments required (Source folder with \*.txt, Destination folder)
