523b65eda47db173959356a5026ce42c147b6d6e
coreyderosa <coreyderosa@gmail.com>
2016-10-22 18:12:32 -0600

    GUI complete with functionality
    GUI and folder and file transfer functions are setup in class.

--- PythonDrill_gui_35.py
+++ PythonDrill_gui_35.py
@@ -11,78 +11,108 @@ import os
 import time
 import shutil
 from glob import glob
-##from tkinter import ttk
-##from tkinter import messagebox
-
-root =Tk()
-
-style  = ttk.Style()
-
-style.theme_use('classic')
-
-#Daily folder Browse Window
-def selectDailyFolder(root):
-    root.dailyFolder = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Daily Folder") #opens dialog box in Documents folder
-    dailyFolder = root.dailyFolder
-    #print(root.dailyFolder)
-    dailyLabel = ttk.Label(root, text = dailyFolder ) #prints selected folder path on label
-    dailyLabel.grid(row = 5, column = 1)
-    dailyLabel.config(foreground = "blue")
 
 
-#Destination folder Browse Window
-def selectDestFolder(root):
-    root.destFolder = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Destination Folder") #opens dialog box in Documents folder
-    destFolder = root.destFolder
-    #print(root.destFolder)
-    destLabel = ttk.Label(root, text = destFolder) #prints selected folder path on label
-    destLabel.grid(row = 5, column = 2, rowspan = 2)
-    destLabel.config(foreground = "blue")
+class FileCheck:
+
+    def __init__(self, master):
+
+        #Frame for header
+        self.frame_header = ttk.Frame(master)
+        self.frame_header.pack()
+        
+        
+        #Daily Folder
+        self.dailyFolderName = StringVar()
+        print (self.dailyFolderName)
+        self.daily = (self.dailyFolderName.get())
+        
+        #Destination Folder
+        self.destFolderName = StringVar()
+        print (self.destFolderName)
+        self.dest = (self.destFolderName.get())
+        
+        #logo and header
+        self.logo = PhotoImage(file = "D:\Documents\School\Tech Academy\Portfolio\coco logo.png")
+        headerLabel = ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, columnspan = 2, pady = 5, sticky = 'w')
+        titleLabel = ttk.Label(self.frame_header, text = 'Welcome to File Check.  File transfer made easy! ', font = 'bold')
+        titleLabel.grid(row = 0, column = 0, columnspan = 2, pady = 5, sticky = 'w')
+
+        #Frame for labels and buttons
+        self.frame_steps = ttk.Frame(master)
+        self.frame_steps.pack()
+
+        #step labels
+        dailyStepLabel = ttk.Label(self.frame_steps, text = 'Step 1- Choose Daily Folder')
+        dailyStepLabel.grid(row = 0, column = 0, columnspan = 2, pady = 5, sticky = 'w')
+        destStepLabel = ttk.Label(self.frame_steps, text = 'Step 2- Choose Destination Folder')
+        destStepLabel.grid(row = 3, column = 0, columnspan = 2, pady = 5, sticky = 'w')
+        initiateStepLabel = ttk.Label(self.frame_steps, text = 'Step 3- Initiate File Check')
+        initiateStepLabel.grid(row = 7, column = 0, columnspan = 2, pady = 5, sticky = 'w')
+        
+        #buttons   
+        dailyButton = ttk.Button(self.frame_steps, text = 'Daily Folder', command = self.selectDailyFolder)
+        dailyButton.grid(row = 1, column = 0, sticky = 'w')
+        destButton = ttk.Button(self.frame_steps, text = 'Destination Folder', command = self.selectDestFolder)
+        destButton.grid(row = 4, column = 0, sticky = 'w')
+        initiateButton = ttk.Button(self.frame_steps, text = 'Initiate', command = lambda: self.timeCompare(self.dailyFileCheck,self.destFileCheck))
+        initiateButton.grid(row = 8, column = 0, sticky = 'w')
+        
+        #path labels
+        self.frame_path = ttk.Frame(master)
+        self.frame_path.pack()
+        dailyPathLabel = ttk.Label(self.frame_steps, text = self.dailyFolderName, textvariable = self.dailyFolderName)
+        dailyPathLabel.grid(row = 1, column = 2, rowspan = 1, sticky = 'W')
+        dailyPathLabel.config(foreground = 'blue')
+        destPathLabel = ttk.Label(self.frame_steps, text = self.destFolderName, textvariable = self.destFolderName)
+        destPathLabel.grid(row = 4, column = 2, rowspan = 1, sticky = 'W')
+        destPathLabel.config(foreground = 'blue')
+   
+
+
+#Daily folder Browse Window        
+    def selectDailyFolder(self):
+        self.dailyFileCheck = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Daily Folder") #opens dialog box in Documents folder
+        self.dailyFolderName.set(self.dailyFileCheck)
+        print (self.dailyFileCheck)
+        print (self.dailyFolderName.get())
+        
+#Destination folder Browse Window        
+    def selectDestFolder(self):
+        self.destFileCheck = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Destination Folder") #opens dialog box in Documents folder
+        self.destFolderName.set(self.destFileCheck)
+        print (self.destFileCheck)
+        print (self.destFolderName.get())
 
 
 #Gets the file's modified time then time 24 hours ago
 #Finds each file's path
 #And if the file's extension = .txt and modified time is larger than 24 hours ago
 #It copies the file to destination folder
-def timeCompare(fileSrc, fileDest):
-    currentTime = datetime.now() #gives us the current time
-    time24hoursAgo = currentTime - timedelta(hours = 24) #finds the time 24 hours from current time
-    for f in os.listdir(fileSrc): #glob shows the entire file path
-        files = os.path.realpath(os.path.join(fileSrc,f)) #shows the file's path then joins the file's path and the directory's paths.  This provides the datetime with the entire file's path
-        if files.endswith('.txt'):
-            fileSrcModifiedTime = datetime.fromtimestamp(os.path.getmtime(files)) #gives us each file's modified time
-            if fileSrcModifiedTime > time24hoursAgo: #if file's modified time is greater than 24 hours old copy file to destination folder
-                print (files, "copied to: ", fileDest)
-                shutil.copy(files,fileDest) #copys file to destination
-            else:
-                print (files, "not copied")
+    def timeCompare(self, dailyFileCheck, destFileCheck):
+        print ('hello {}'.format(dailyFileCheck)) #checks to see if the daily folder name is passing to this function
+        print ('hello {}'.format(destFileCheck)) #checks to see if the dest folder name is passing to this function
+        print ()
+        currentTime = datetime.now() #gives us the current time
+        time24hoursAgo = currentTime - timedelta(hours = 24) #finds the time 24 hours from current time
+        for f in os.listdir(dailyFileCheck): #listdir lists each file in the directory
+            files = os.path.realpath(os.path.join(dailyFileCheck,f)) #shows the file's path then joins the file's path and the directory's paths.  This provides the datetime with the entire file's path
+            if files.endswith('.txt'):
+                fileSrcModifiedTime = datetime.fromtimestamp(os.path.getmtime(files)) #gives us each file's modified time
+                if fileSrcModifiedTime > time24hoursAgo: #if file's modified time is greater than 24 hours old copy file to destination folder
+                    print (files, "copied to: ", destFileCheck)
+                    shutil.copy(files,destFileCheck) #copys file to destination
+                else:
+                    print (files, "not copied")
+                    
 
 
-#Buttons
-dailyButton = ttk.Button(root, text = "Choose the 'Checked Daily' folder...", command = lambda: selectDailyFolder(root))
-dailyButton.grid(row = 3, column = 1, rowspan = 2)
+def main():
+    root = Tk() #needed for tkinter
+    root.wm_title("File Check")
+    root.minsize(400, 280)
+    filecheck = FileCheck(root) #sets the class up with root from tkinter
+    root.mainloop() #needed for tkinter
 
-destButton = ttk.Button(root, text = "Choose the 'Destination' folder...", command = lambda: selectDestFolder(root))
-destButton.grid(row = 3, column = 2, rowspan = 2)
-
-initiateButton = ttk.Button(root, text = "'File Check'" , command = lambda: timeCompare(dailyFolder, destFolder))
-initiateButton.grid(row = 3, column = 3, rowspan = 2)
-
-#Labels
-dailyFolder = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Daily Folder") #opens dialog box in Documents folder
-dailyLabel = ttk.Label(root, text = dailyFolder ) #prints selected folder path on label
-dailyLabel.grid(row = 5, column = 1)
-dailyLabel.config(foreground = "blue")
-
-destFolder = filedialog.askdirectory(initialdir = "D:\Documents", title = "Select your Destination Folder") #opens dialog box in Documents folder
-destLabel = ttk.Label(root, text = destFolder) #prints selected folder path on label
-destLabel.grid(row = 5, column = 2, rowspan = 2)
-destLabel.config(foreground = "blue")
-
-
-#fileSrc = r'D:\Documents\Folder A\*txt'
-#fileDest = r'D:\Documents\Folder B'
-##timeCompare(fileSrc, fileDest) #2 arguments required (Source folder with \*.txt, Destination folder)
-
-root.mainloop()
+if __name__ == '__main__' : main() #runs the main function which runs the class and functions
 
